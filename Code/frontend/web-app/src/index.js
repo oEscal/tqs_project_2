b72a532e03b0298cc7bbbab3b401b1f1c444a71a
Rafaelyot <rafaeljsimoes@gmail.com>
2020-05-14 06:33:13 +0100

    Done

--- Code/frontend/web-app/src/index.js
+++ Code/frontend/web-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import {createBrowserHistory} from "history";
+import {Router, Route, Switch} from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
@@ -13,20 +13,22 @@ import LoginPage from "views/LoginPage/LoginPage.js";
 
 import GameSearch from "views/GameSearch/GameSearch.js";
 import HomePage from "views/HomePage/HomePage.js";
+import Cart from "views/Cart/Cart.js";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/components" component={Components} />
+    <Router history={hist}>
+        <Switch>
+            <Route path="/landing-page" component={LandingPage}/>
+            <Route path="/profile-page" component={ProfilePage}/>
+            <Route path="/login-page" component={LoginPage}/>
+            <Route path="/components" component={Components}/>
 
-      <Route path="/games" component={GameSearch} />
-      <Route path="/" component={HomePage} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+            <Route path="/cart" component={Cart}/>
+            <Route path="/games" component={GameSearch}/>
+            <Route path="/" component={HomePage}/>
+        </Switch>
+    </Router>,
+    document.getElementById("root")
 );
