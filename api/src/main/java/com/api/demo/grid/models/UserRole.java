44ed7fc576905fe045a8e553e753da118702c814
escaleira <pedroescaleira@hotmail.com>
2020-05-16 16:59:42 +0100

    user roles table

---
+++ api/src/main/java/com/api/demo/grid/models/UserRole.java
@@ -0,0 +1,44 @@
+package com.api.demo.grid.models;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
