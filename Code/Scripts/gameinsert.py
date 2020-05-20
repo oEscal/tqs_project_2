ae81c954237073e8be3307f5972f829cf4591d6e
DrPunpun <pedrooliveira99@ua.pt>
2020-05-20 14:36:53 +0100

    added script to insert games

--- Code/Scripts/gameinsert.py
+++ Code/Scripts/gameinsert.py
@@ -4,13 +4,15 @@ import requests
 base_url = "https://api.rawg.io/api/"
 grid_url = "http://localhost:8080/grid/"
 
+headers = {'Content-type': 'application/json'}
+
 url = base_url + "developers"
 count = 0
 while count < 100 and url:
 	print(url)
 	response = requests.get(url).json()
 	for dev in response["results"]:
-		requests.post(grid_url + "developer", data={"name": dev["name"]})
+		requests.post(grid_url + "developer", data=json.dumps({"name": dev["name"]}), headers=headers)
 		count += 1
 	url = response["next"]
 
@@ -21,8 +23,8 @@ while count < 50 and url:
 	print(url)
 	response = requests.get(url).json()
 	for genre in response["results"]:
-		genre_info = requests.get(base_url + "genres/" + str(genre["id"]))
-		requests.post(grid_url + "genre", data={"name": genre_info["name"], "description": genre_info["description"]})
+		genre_info = requests.get(base_url + "genres/" + str(genre["id"])).json()
+		requests.post(grid_url + "genre", data=json.dumps({"name": genre_info["name"], "description": genre_info["description"]}), headers=headers)
 		count += 1
 	url = response["next"]
 
@@ -33,8 +35,8 @@ while count < 100 and url:
 	print(url)
 	response = requests.get(url).json()
 	for genre in response["results"]:
-		pub_info = requests.get(base_url + "publishers/" + str(genre["id"]))
-		requests.post(grid_url + "publisher", data={"name": pub_info["name"], "description": pub_info["description"]})
+		pub_info = requests.get(base_url + "publishers/" + str(genre["id"])).json()
+		requests.post(grid_url + "publisher", data=json.dumps({"name": pub_info["name"], "description": pub_info["description"]}), headers=headers)
 		count += 1
 	url = response["next"]
 
@@ -45,14 +47,18 @@ while count < 300:
 	print(url)
 	response = requests.get(url).json()
 	for genre in response["results"]:
-		game_info = requests.get(base_url + "games/" + str(genre["id"]))
-		requests.post(grid_url + "game", data={
-			"name": game_info["name"],
-			"description": game_info["description"],
-			"releaseDate": game_info["released"],
-			"coverUrl": game_info["background_image"],
-			"developers": [dev["name"] for dev in game_info["developers"]],
-			"gameGenres": [genre["name"] for genre in game_info["genres"]],
-			"publisher": game_info["publishers"][0]["name"]
-		})
+		game_info = requests.get(base_url + "games/" + str(genre["id"])).json()
+		try:
+			requests.post(grid_url + "game", data=json.dumps({
+				"name": game_info["name"],
+				"description": game_info["description"],
+				"releaseDate": game_info["released"],
+				"coverUrl": game_info["background_image"],
+				"developers": [dev["name"] for dev in game_info["developers"]],
+				"gameGenres": [genre["name"] for genre in game_info["genres"]],
+				"publisher": game_info["publishers"][0]["name"]
+			}), headers=headers)
+			count += 1
+		except Exception:
+			continue
 	url = response["next"]
--- api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
+++ api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
@@ -17,12 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
