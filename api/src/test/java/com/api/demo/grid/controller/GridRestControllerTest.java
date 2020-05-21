5c7bf792565891691bd40c08923bd632f1e029ef
escaleira <pedroescaleira@hotmail.com>
2020-05-21 23:05:01 +0100

    try again

--- api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
+++ api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +26,7 @@ import java.util.Arrays;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 class GridRestControllerTest {
 
     @Autowired
