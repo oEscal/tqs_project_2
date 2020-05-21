b30b2f749a14dfa3043f23c5104d448ce25fd334
escaleira <pedroescaleira@hotmail.com>
2020-05-21 22:59:46 +0100

    fixes on GridRestControllerTest

--- api/src/test/java/com/api/demo/grid/controller/GridRestControllerIT.java
+++ api/src/test/java/com/api/demo/grid/controller/GridRestControllerIT.java
@@ -29,9 +29,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
 class GridRestControllerIT {
     @Autowired
     private MockMvc mockMvc;
--- api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
+++ api/src/test/java/com/api/demo/grid/controller/GridRestControllerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,8 +22,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Arrays;
 
-@WebMvcTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class GridRestControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
