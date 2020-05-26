88ec3f5360c19d37a186eb981c28b4353a6c4550
escaleira <pedroescaleira@hotmail.com>
2020-05-26 05:48:11 +0100

    it doesnt work yet

--- api/src/main/java/com/api/demo/grid/config/SpringSecurityConfig.java
+++ api/src/main/java/com/api/demo/grid/config/SpringSecurityConfig.java
@@ -53,7 +53,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception {
         // TODO -> verify roles here
         http.cors().and().csrf().disable()
                 .authorizeRequests()
