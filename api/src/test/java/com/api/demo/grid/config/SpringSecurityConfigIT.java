870230fbceb3a5853691371791f288d7ae2f5879
escaleira <pedroescaleira@hotmail.com>
2020-05-26 01:59:06 +0100

    spring security tests -> whenAccessNonAdminUrlWithTypicalUser_accessIsUnauthorized (doesnt pass because tdd)

--- api/src/test/java/com/api/demo/grid/config/SpringSecurityConfigIT.java
+++ api/src/test/java/com/api/demo/grid/config/SpringSecurityConfigIT.java
@@ -6,6 +6,8 @@ import com.api.demo.grid.models.User;
 import com.api.demo.grid.repository.UserRepository;
 import com.api.demo.grid.service.UserService;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -16,9 +18,13 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
+import java.text.SimpleDateFormat;
+
+import static com.api.demo.grid.utils.UserJson.simpleUserJson;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -54,19 +60,39 @@ class SpringSecurityConfigIT {
             mBirthDateStr = "17/10/2010";
 
 
+    @BeforeEach
+    @SneakyThrows
+    void setup() {
+        this.mSimpleUser = new User();
+        this.mSimpleUser.setUsername(mUsername1);
+        this.mSimpleUser.setName(mName1);
+        this.mSimpleUser.setEmail(mEmail1);
+        this.mSimpleUser.setCountry(mCountry1);
+        this.mSimpleUser.setPassword(mPassword1);
+        this.mSimpleUser.setBirthDate(new SimpleDateFormat("dd/MM/yyyy").parse(mBirthDateStr));
+
+        this.mSimpleUserJson = simpleUserJson(mUsername1, mPassword1, mBirthDateStr, mEmail1, mCountry1, mName1);
+
+        this.mSimpleUserDTO = new UserDTO(mUsername1, mName1, mEmail1, mCountry1, mPassword1,
+                new SimpleDateFormat("dd/MM/yyyy").parse(mBirthDateStr));
+    }
+
+    @AfterEach
+    void afterEach() {
+        mUserRepository.deleteAll();
+    }
+
+
+    /***
+     * Non logged in user accesses
+     ***/
     @Test
     @SneakyThrows
     void whenAccessOpenUrlWithoutLoggedIn_accessIsNotUnauthorized() {
 
         String[] authWhitelist = (String[]) ReflectionTestUtils.getField(mSpringSecurityConfig, "AUTH_WHITELIST");
 
-        for (String endPoint : authWhitelist) {
-            RequestBuilder request = post(endPoint).contentType(MediaType.APPLICATION_JSON);
-
-            int returnedStatus = mMvc.perform(request).andReturn().getResponse().getStatus();
-            assertNotEquals(401, returnedStatus, "Endpoint " + endPoint + " should be authorized for " +
-                    "any user");
-        }
+        verifyEndpointsAreAuthorized(authWhitelist);
     }
 
     @Test
@@ -82,6 +108,42 @@ class SpringSecurityConfigIT {
 
         // verify for user endpoints
         verifyEndpointsAreUnauthorized(userWhitelist);
+    }
+
+
+    /***
+     * Logged in typical user accesses
+     ***/
+    @Test
+    @SneakyThrows
+    void whenAccessNonAdminUrlWithTypicalUser_accessIsUnauthorized() {
+
+        // login with typical user (non admin)
+        mUserService.saveUser(mSimpleUserDTO);
+        RequestBuilder request = post("/grid/login").with(httpBasic(mUsername1, mPassword1));
+        mMvc.perform(request).andExpect(status().isOk())
+                .andExpect(jsonPath("$.admin", is(false)));
+
+        String[] authWhitelist = (String[]) ReflectionTestUtils.getField(mSpringSecurityConfig, "AUTH_WHITELIST");
+        String[] userWhitelist = (String[]) ReflectionTestUtils.getField(mSpringSecurityConfig, "USER_WHITELIST");
+
+        // verify for open endpoints
+        verifyEndpointsAreAuthorized(authWhitelist);
+
+        // verify for user endpoints
+        verifyEndpointsAreAuthorized(userWhitelist);
+    }
+
+
+    @SneakyThrows
+    private void verifyEndpointsAreAuthorized(String[] authList) {
+
+        for (String endPoint : authList) {
+            RequestBuilder request = post(endPoint).contentType(MediaType.APPLICATION_JSON);
+
+            int returnedStatus = mMvc.perform(request).andReturn().getResponse().getStatus();
+            assertNotEquals(401, returnedStatus, "Endpoint " + endPoint + " should be unauthorized");
+        }
     }
 
     @SneakyThrows
