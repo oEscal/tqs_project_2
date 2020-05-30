bb669a02e1c3ccc0bfa1b29677af2b2560bbf37d
escaleira <pedroescaleira@hotmail.com>
2020-05-30 22:54:22 +0100

    imports

--- api/src/main/java/com/api/demo/grid/models/Buy.java
+++ api/src/main/java/com/api/demo/grid/models/Buy.java
@@ -10,7 +10,18 @@ import lombok.ToString;
 import lombok.NoArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 import java.util.Objects;
 
