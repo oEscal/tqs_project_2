582fa4b2e2ed1ea95a242e9f684ed8a693eaca30
escaleira <pedroescaleira@hotmail.com>
2020-05-31 04:31:02 +0100

    controller for delete user!

--- api/src/main/java/com/api/demo/grid/controller/Account.java
+++ api/src/main/java/com/api/demo/grid/controller/Account.java
@@ -7,16 +7,13 @@ import com.api.demo.grid.models.User;
 import com.api.demo.grid.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 
 @RestController
+@RequestMapping("/grid")
 @CrossOrigin
 public class Account {
 
@@ -24,18 +21,24 @@ public class Account {
     private UserService mUserService;
 
 
-    @PostMapping("/grid/sign-up")
+    @PostMapping("/sign-up")
     public User createUser(@Valid @RequestBody UserDTO user) throws ExceptionDetails {
 
         return mUserService.saveUser(user);
     }
 
-    @PostMapping("/grid/login")
+    @PostMapping("/login")
     public ResponseEntity<User> login(@RequestHeader("Authorization") String auth) {
 
         String value = ControllerUtils.getUserFromAuth(auth);
         User user = mUserService.getUser(value);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/remove-user")
+    public void removeUser(@RequestHeader("Authorization") String auth, @RequestParam String username) {
+
+        mUserService.deleteUser(username);
     }
 }
