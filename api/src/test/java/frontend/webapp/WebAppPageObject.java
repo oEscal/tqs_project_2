05ceb9c305d905ae757c83d60ad96d4fca38c001
escaleira <pedroescaleira@hotmail.com>
2020-06-01 01:34:58 +0100

    first try

--- api/src/test/java/frontend/webapp/WebAppPageObject.java
+++ api/src/test/java/frontend/webapp/WebAppPageObject.java
@@ -1,8 +1,13 @@
 package frontend.webapp;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
