c9d3f588f5dc12bfa6fa49414a7880540456f9ce
DrPunpun <pedrooliveira99@ua.pt>
2020-06-01 19:18:05 +0100

    fixed imports

--- api/src/main/java/com/api/demo/grid/service/UserService.java
+++ api/src/main/java/com/api/demo/grid/service/UserService.java
@@ -12,7 +12,6 @@ import com.api.demo.grid.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Optional;
