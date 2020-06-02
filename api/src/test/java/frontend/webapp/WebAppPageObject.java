dafb8f9bcc3411891888da4c2ec2f3076c21d6db
HerouFenix <diogo04@ua.pt>
2020-06-02 15:33:41 +0100

    done

--- api/src/test/java/frontend/webapp/WebAppPageObject.java
+++ api/src/test/java/frontend/webapp/WebAppPageObject.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
